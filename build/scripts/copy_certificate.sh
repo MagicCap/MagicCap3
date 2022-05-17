3d3a49649b2d014dc0c7fea78b6f895a62d6fc82
Jake Gealer <jake.gealer@krystal.uk>
2022-05-17 21:43:19 +0100

    circle > runner?

--- build/scripts/copy_certificate.sh
+++ build/scripts/copy_certificate.sh
@@ -5,11 +5,11 @@ set -e
 
 KEY_CHAIN=~/certs.keychain
 
-security create-keychain -p circle $KEY_CHAIN
+security create-keychain -p runner $KEY_CHAIN
 # Make the keychain the default so identities are found
 security default-keychain -s $KEY_CHAIN
 # Unlock the keychain
-security unlock-keychain -p circle $KEY_CHAIN
+security unlock-keychain -p runner $KEY_CHAIN
 # Set keychain locking timeout to 3600 seconds
 security set-keychain-settings -t 3600 -u $KEY_CHAIN
 
